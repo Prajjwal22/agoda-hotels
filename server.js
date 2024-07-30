6e5e00b417f049f5432033570d853737777befa5
prajjwal <howtoshout@gmail.com>
2024-07-30 22:26:57 +0530

    done

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const axios = require("axios");
+const app = express();
+const cors = require("cors");
+const port = 3000;
+app.use(express.json());
+
+const corsOptions = {
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  allowedHeaders: "Content-Type,Authorization",
+};
+
+app.use(cors(corsOptions));
+
+app.get("/hotels", async (req, res) => {
+  console.log(req);
+  try {
+    const {
+      searchText,
+      guid,
+      origin,
+      cid,
+      pageTypeId,
+      logtime,
+      logTypeId,
+      isHotelLandSearch,
+    } = req.query;
+
+    console.log(searchText, "pradjjwal");
+
+
+
+    const url = `https://www.agoda.com/api/cronos/search/GetUnifiedSuggestResult/3/16/1/0/en-gb/?searchText=${searchText}&guid=${guid}&origin=${origin}&cid=${cid}&pageTypeId=${pageTypeId}&logtime=${logtime}&logTypeId=${logTypeId}&isHotelLandSearch=${isHotelLandSearch}`;
+
+    const response = await axios.get(url);
+
+    res.json(response.data.ViewModelList);
+  } catch (error) {
+    console.error("Error fetching hotel data:", error);
+    res.status(500).send("Internal Server Error", error);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
